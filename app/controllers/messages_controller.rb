0690a4f0ce2f912f8547b7175a13e66cc555c184
taniatsu <mjp569.gj3@gmail.com>
2019-12-17 20:33:41 +0900

    add

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,6 +4,8 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @messages = @group.messages.includes(:user)
+    @group_name = @group.group_name
+    @members = @group.users
   end
 
   def create
