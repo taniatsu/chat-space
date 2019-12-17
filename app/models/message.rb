c0c906f5ab97dcae07b6f2331d2649e13d63dd63
taniatsu <mjp569.gj3@gmail.com>
2019-12-17 21:47:40 +0900

    add

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,6 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :group
-
-  validates :body, presence: true, unless: :image?
-
   mount_uploader :image, ImageUploader
+  validates :body, presence: true, unless: :image?
 end
