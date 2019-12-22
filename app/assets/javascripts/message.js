3c6449e3c4d1a95b0ee7e9a4d3ce916000208e21
taniatsu <mjp569.gj3@gmail.com>
2019-12-22 20:11:05 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -46,24 +46,24 @@ $(function(){
 $('#new_message').on('submit', function(e){
   e.preventDefault();
   var formData = new FormData(this);
-  var url = $(this).attr('action')
-  $.ajax({
-    url: url,
-    type: "POST",
-    data: formData,
-    dataType: 'json',
-    processData: false,
-    contentType: false
-  })
-    .done(function(data){
-      var html = buildHTML(data);
-      $('.chat-main__message-list').append(html);
-      $('form')[0].reset();
-      $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
-      $(".message-form__btn").prop('disabled', false);
+    var url = $(this).attr('action')
+      $.ajax({
+        url: url,
+        type: "POST",
+        data: formData,
+        dataType: 'json',
+        processData: false,
+        contentType: false
+      })
+        .done(function(data){
+          var html = buildHTML(data);
+          $('.chat-main__message-list').append(html);
+          $('form')[0].reset();
+          $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
+          $(".message-form__btn").prop('disabled', false);
+        })
+        .fail(function() {
+          alert("メッセージ送信に失敗しました");
+      });
     })
-    .fail(function() {
-      alert("メッセージ送信に失敗しました");
-  });
-  })
 });
